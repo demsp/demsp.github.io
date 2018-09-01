0e84b5e09e0da28b663cb61799b2a8a4e541a7c2
Dima <dserp18@gmail.com>
2018-09-01 10:51:50 +0300

    Update server1.py

--- server1.py
+++ server1.py
@@ -1,11 +1,12 @@
 from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
 
 class HttpProcessor(BaseHTTPRequestHandler):
-def do_GET(self):
-self.send_response(200)
-self.send_header('content-type','text/html')
-self.end_headers()
-self.wfile.write("hello !")
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('content-type','text/html')
+        self.end_headers()
+        self.wfile.write("hello ABC!")
+
 
 serv = HTTPServer(("localhost",80),HttpProcessor)
 serv.serve_forever()
