920dccedf879520d2b3091395f9993c35bedf415
demsp <dserp18@gmail.com>
2019-04-15 07:57:24 +0300

    Update rect.js

--- js/rect.js
+++ js/rect.js
@@ -4,7 +4,7 @@ function draw() {
   if (canvas.getContext) {
     var ctx = canvas.getContext('2d');
 
-    ctx.fillRect(25, 25, 500, 500);
+    ctx.fillRect(25, 25, 250, 250);
     ctx.clearRect(45, 45, 60, 60);
     ctx.strokeRect(50, 50, 50, 50);
   }
